389dc5c6c64813308135db285cb2e8cff4dd4329
Squidly271 <unraidsquid@gmail.com>
2025-06-15 20:40:22 -0400

    test publish

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -1,1 +1,1 @@
-cp /usr/local/emhttp/plugins/theme.engine/* /mnt/user/temp/unraid-theme-engine/source/theme.engine/usr/local/emhttp/plugins/theme.engine -R -v -p
+cp /usr/local/emhttp/plugins/theme.engine/* /tmp/GitHub/unraid-theme-engine/source/theme.engine/usr/local/emhttp/plugins/theme.engine -R -v -p
--- pkg_build.sh
+++ pkg_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd source/theme.engine
-pkg_build.sh
+./pkg_build.sh
